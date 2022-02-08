697c0813be8f97bdb70b81530f5fb660e48668f6
gus <75089662+glevinson@users.noreply.github.com>
2022-02-08 16:01:47 +0000

    Added PowerOn & PowerOff

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -1,17 +1,25 @@
 package ic.doc.camera;
 
-public class Camera {
+public class Camera{
 
-  public void pressShutter() {
-    // not implemented
+  private final Sensor sensor;
+  boolean cameraOn = false;
+
+  public Camera(Sensor sensor) {
+    this.sensor = sensor;
   }
 
   public void powerOn() {
+    cameraOn = true;
+    sensor.powerUp();
     // not implemented
   }
 
   public void powerOff() {
+    cameraOn = false;
+    sensor.powerDown();
     // not implemented
   }
+
 }
 
--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,5 +1,6 @@
 package ic.doc.camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,10 +10,28 @@ public class CameraTest {
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
+  Sensor sensor = context.mock(Sensor.class);
+  Camera camera = new Camera(sensor);
+
+
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
+    context.checking(new Expectations() {{
+      exactly(1).of(sensor).powerUp();
+    }});
 
     // write your test here
+    camera.powerOn();
+  }
+
+  @Test
+  public void switchingTheCameraOffPowersDownTheSensor() {
+    context.checking(new Expectations() {{
+      exactly(1).of(sensor).powerDown();
+    }});
+
+    // write your test here
+    camera.powerOff();
   }
 
 }
